f3c98ed261cab115ef466864041ee8745e7f1785
Padon Mungsanti <padon.m@mscsittipol.com>
2019-02-03 17:00:53 +0700

    Apply middleware redux-thunk

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import App from './containers/App'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import { searchRobots } from './reducers'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { searchRobots, requestRobots } from './reducers'
 import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
 
 const logger = createLogger()
-const store = createStore(searchRobots, applyMiddleware(logger))
+const rootReducer = combineReducers({
+    searchRobots,
+    requestRobots
+})
+const store = createStore(rootReducer, applyMiddleware(
+    thunkMiddleware,
+    logger
+))
 
 ReactDOM.render(
                 <Provider store={store}>
